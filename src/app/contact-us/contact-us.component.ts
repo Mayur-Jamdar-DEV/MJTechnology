8b612ed02bcb7d568f3987a0c2e4b71bd15bbe41
mayur5152 <jamdarmayur5152@gmail.com>
2023-01-29 14:12:06 +0530

    contact-us form

--- src/app/contact-us/contact-us.component.ts
+++ src/app/contact-us/contact-us.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-us',
@@ -9,13 +11,39 @@ export class ContactUsComponent implements OnInit {
   latitude = 51.678418;
   longitude = 7.809007;
 
-  constructor() { }
+  public signUpForm !: FormGroup;
+  mobileNumber : number;
+
+  constructor(private formBuilder : FormBuilder, private http : HttpClient) {
+    this.mobileNumber = 0;
+   }
 
   ngOnInit(): void {
+    this.signUpForm = this.formBuilder.group({
+      name:['', Validators.required],
+      email:['', [Validators.required, Validators.email]],
+      phoneNumber:['', [Validators.required, Validators.pattern("^((\\+91-?)|0)?[0-9]{10}$")]],
+      message:['', Validators.required]
+    })
   }
 
-  onSubmit(){
-    
+  signUp(){
+    this.http.post<any>("http://localhost:3000/signupUsers", this.signUpForm.value)
+    .subscribe(res=>{
+      alert("Message send succesful");
+      this.signUpForm.reset();
+    })
   }
+  get mobno(){
+    return this.signUpForm.controls;
+  }
+  get emailid(){
+    return this.signUpForm.controls;
+  }
+
+  doSubmit(){
+    console.log(this.signUpForm.value);
+  }
+
 
 }
